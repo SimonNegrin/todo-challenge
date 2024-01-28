29cb12643538ec9fe55351f93d75199b4aef6c3b
Simón Negrín <simon.negrin@iskaypet.com>
2024-01-28 13:56:52 +0000

    Added page to create task

---
+++ src/lib/pages/CreateTask.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TaskForm from '../components/TaskForm.svelte'
+
+</script>
+
+<h1 class="p-4 pb-0 text-xl">Crear nueva tarea</h1>
+<TaskForm />
--- src/lib/pages/MyTasks.svelte
+++ src/lib/pages/MyTasks.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { Route, Router } from 'svelte-routing'
   import TasksList from '../components/TasksList.svelte'
-  import TaskForm from '../components/TaskForm.svelte'
+  import CreateTask from './CreateTask.svelte'
 
 </script>
 
 <Router basepath="/my-tasks">
   <Route path="/" component={TasksList} />
-  <Route path="/create" component={TaskForm} />
+  <Route path="/create" component={CreateTask} />
 </Router>
